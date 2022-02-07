57b88df0ed8646e51e488bf80b4c54591a68c3a2
tomineodegard-kea <tomi0105@stud.kea.dk>
2022-02-07 17:37:50 +0100

    made consts to start of js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 `use strict`;
 
+const inputText = document.querySelector("#input_text");
+const outpuText = document.querySelector("#output_text");
+const operator = document.querySelector("#operator");
+const generateBtn = document.querySelector("#generate");
+
 window.addEventListener("DOMContentLoaded", start);
 
 function start() {}
